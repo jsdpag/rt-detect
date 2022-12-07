66c1b9646ea8598c9a9fb820a9cd783ee4c0ebf8
smithj <jackson.smith@esi-frankfurt.de>
2022-12-07 17:21:52 +0100

    Enable unique EDF files on EyeLink Host PC for each session ID.

--- session_launch_script.m
+++ session_launch_script.m
@@ -3,10 +3,17 @@
 % session_launch_script.m
 % 
 % Task-specific launch setting actions go here. Define sesslaunchparams for
-% special actions.
+% special actions. It is an optional struct with optional fields:
+% 
+%   .EyeServer_SampleMode - If true then sample mode is enabled.
+%   .EyeServer_UniqueTmp  - If true then each session number gets its own
+%     tmp<session id>.edf file on the EyeLink host PC. Thus, make sure that
+%     each session gets a unique id on a given day.
 % 
 
 % Don't open normal ARCADE control screen. Use the minimalist remote,
 % instead.
 cfg.ControlScreen = 'makeArcadeRemote.m' ;
 
+% Create sesslaunchparams
+sesslaunchparams.EyeServer_UniqueTmp = true ;
